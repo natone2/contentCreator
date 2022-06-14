069a55f9694429262d2625af872956d8105e743c
Álex Villalba <natone@riseup.net>
2022-06-14 18:58:39 +0200

    Delete createArticle.php

--- createArticle.php
+++
@@ -1,6 +0,0 @@
-<?php        
-        $date = $row['date'];
-        $file = fopen($date'.html', "a") or die("No se pudo crear el post");
-        fwrite($file, "<h2>$row[uid]<h2>");
-        fclose($file);
-?>        
